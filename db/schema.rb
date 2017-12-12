67ddaf3beae6ec20be27cffe06d31c375459f3d5
praveen kumar <praveenkumar.s.14june@gmail.com>
2017-12-12 11:33:18 +0000

    add recipe and chef validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171212052523) do
+ActiveRecord::Schema.define(version: 20171212105520) do
 
-  create_table "recipes", force: :cascade do |t|
-    t.string   "name"
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
