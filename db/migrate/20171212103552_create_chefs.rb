f017573c45a5246b944f8003c8c300d57ca208d0
praveen kumar <praveenkumar.s.14june@gmail.com>
2017-12-18 06:52:53 +0000

    final

--- db/migrate/20171212103552_create_chefs.rb
+++ db/migrate/20171212103552_create_chefs.rb
@@ -3,6 +3,7 @@ class CreateChefs < ActiveRecord::Migration[5.0]
     create_table :chefs do |t|
       t.string :chefname
       t.string :email
+      t.string :password
       t.timestamps
     end
   end
